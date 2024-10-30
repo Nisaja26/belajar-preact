308b8bad75f11ca03af37d64a8a3089ab46e4255
Nisaja26 <nissaikaracmawati@gmail.com>
2024-10-30 21:58:43 +0700

    bagian 4 80%

---
+++ pages/home/index.jsx
@@ -0,0 +1,20 @@
+export default function Home() {
+    return (
+      <div className="container mt-5 mb-5">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card border-0 rounded shadow">
+              <div className="p-5 bg-light rounded-3">
+                <div className="container-fluid py-5">
+                  <h1 className="display-5 fw-bold">PREACT + LARAVEL 11</h1>
+                  <p className="col-md-8 fs-4">
+                    Belajar CRUD dengan Preact dan Laravel 11 di SantriKoding.com
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
